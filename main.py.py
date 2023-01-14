177fd8177c89cdb49e0b92f123939509ace2c7b8
mahammads <mahammadjubersarwad@gmail.com>
2023-01-14 22:33:14 +0530

    new changes updated

--- main.py.py
+++ main.py.py
@@ -5,46 +5,17 @@ from sftp_module import SftpModule
 from PIL import ImageTk, Image
 
 base = Tk()  
-base.geometry("450x400")  
+base.geometry("500x400")  
 base.title("SFTP")  
 
 title = Label(base, text="SFTP DATA TRANSFER", width=30,font=("bold",12),fg='brown',bg ='white')   
 title.place(x=80,y=10) 
+
 file_name = ''
 folder_name = ''
 
-def browse_file():
-    global file_name
-    file = filedialog.askopenfilename()
-    file_name = file
-    lbl_file_name = Label(base, text=file_name, width=60, font=("arial",8),fg='blue',borderwidth=2, relief="raised",justify= LEFT)  
-    lbl_file_name.place(x=40,y=200)
-
-def browse_folder():
-    global file_name
-    folder = filedialog.askdirectory()
-    folder_name = folder
-    lbl_file_name = Label(base, text=folder_name, width=60, font=("arial",8),fg='blue',borderwidth=2, relief="raised",justify= LEFT)  
-    lbl_file_name.place(x=40,y=200)
-
-def sftp_upload():
-    myclick()
-    pass
-
-def sftp_download():
-    pass
-
-def show():
-   p = pswd_entry.get()
-   Label(base, text= str(p)).place(x=330, y=100)
-
-image=Image.open('password_icon.jpg')
-img=image.resize((15, 15))
-pw_img = ImageTk.PhotoImage(img)
-
-Button(base, text="",image=pw_img,command=show).place(x=310,y=100)
-
-horizontal =Frame(base, bg='black', height=1,width=440)
+# -------------------------------------all labels and entry fields---------------------------------------------
+horizontal =Frame(base, bg='black', height=1,width=540)
 horizontal.place(x=5, y=300)
 
 sftp_host= Label(base, text="Hostname:", width=10, font=("arial",10))  
@@ -69,7 +40,7 @@ port_entry.place(x=180, y=125)
 
 local_file= Label(base, text="Local File/Folder", width=15, font=("arial",10))  
 local_file.place(x=40, y=200)  
-local_entry= Entry(base,width=40)  
+local_entry= Entry(base,width=40) 
 local_entry.place(x=180, y=200) 
 
 sftp_file= Label(base, text="Remote File/Folder", width=15, font=("arial",9))  
@@ -77,35 +48,102 @@ sftp_file.place(x=40, y=170)
 file_entry= Entry(base,width=40)  
 file_entry.place(x=180, y=170)  
 
+# ---------------------------all function declaration----------------------------------------------
+def browse_file():
+    global file_name
+    file = filedialog.askopenfilename()
+    file_name = file
+    local_file= Label(base, text="Local File/Folder", width=15, font=("arial",10))  
+    local_file.place(x=40, y=200)  
+    local_entry= Entry(base,width=40) 
+    local_entry.place(x=180, y=200) 
+    local_entry.insert(0, file_name)
 
-def myclick():
-    print(file_name)
+def browse_folder():
+    global folder_name
+    folder = filedialog.askdirectory()
+    folder_name = folder
+    local_file= Label(base, text="Local File/Folder", width=15, font=("arial",10))  
+    local_file.place(x=40, y=200)  
+    local_entry= Entry(base,width=40) 
+    local_entry.place(x=180, y=200) 
+    local_entry.insert(0, folder_name)
+
+
+def sftp_upload():
+    sftp_func()
+    pass
+
+def sftp_download():
+    pass
+
+def sftp_delete():
+    pass
+
+def close():
+    base.quit()
+
+def show():
+   p = pswd_entry.get()
+   Label(base, text= str(p)).place(x=330, y=100)
+
+
+def reset_path():
+    global file_name
+    global folder_name
+    file_name = ''
+    folder_name = ''
+    local_file= Label(base, text="Local File/Folder", width=15, font=("arial",10))  
+    local_file.place(x=40, y=200)  
+    local_entry= Entry(base,width=40) 
+    local_entry.place(x=180, y=200) 
+
+def sftp_func(upload=False, download=False, delete=False):
+    if (file_name =='') and (folder_name!=''):
+        local_file_name = folder_name
+    elif (file_name !='') and (folder_name ==''):
+        local_file_name = file_name
+    elif (file_name !='') and (folder_name !=''):
+        local_file_name= file_name
+  
     host_name = host_entry.get()
     user_name = user_entry.get()
     password = pswd_entry.get()
     port = port_entry.get()
 
-    print(host_name)
-    print(user_name)
-    print(password)  
-    print(port)  
+    # print(host_name)
+    # print(user_name)
+    # print(password)  
+    # print(port)  
+    # print(local_file_name)
+
     try:
-        obj = SftpModule(host_name,user_name,password,int(port))
+        obj = SftpModule(host_name, user_name, password, int(port))
+        
     except ValueError as e:
         # raise e
         error_lbl = Label(base, text="Error: Values missing in input field", width=40,font=("arial",10))
         error_lbl.place(x=50, y=310) 
 
- 
+# -------------------------------------all button configuration---------------------------------------------
+image=Image.open('reset_2.jpg')
+img=image.resize((15, 15))
+reset_img = ImageTk.PhotoImage(img)
+Button(base, text="",image=reset_img,command=reset_path).place(x=430,y=200)
+
+image=Image.open('password_icon.jpg')
+img=image.resize((15, 15))
+pw_img = ImageTk.PhotoImage(img)
+Button(base, text="",image=pw_img,command=show).place(x=310,y=100)
+
 Button(base, text='Browse File', width=12,font=("arial",9),command = lambda:browse_file(), fg='white',bg ='brown').place(x=110,y=240)
 lbl_or= Label(base, text="or", width=2, font=("arial",10))  
 lbl_or.place(x=205, y=240)  
 Button(base, text='Browse Folder', width=12,font=("arial",9),command = lambda:browse_folder(), fg='white',bg ='brown').place(x=225,y=240) 
 
-
-
-Button(base, text="Upload", command= sftp_upload, width=12,bg="brown",fg='white').place(x=40,y=320)
-Button(base, text="Download", command= sftp_download, width=12,bg="brown",fg='white').place(x=150,y=320)
-Button(base, text="Delete", command= sftp_download, width=12,bg="brown",fg='white').place(x=260,y=320)
+Button(base, text="Upload", command= sftp_upload, width=12,bg="brown",fg='white').place(x=30,y=320)
+Button(base, text="Download", command= sftp_download, width=12,bg="brown",fg='white').place(x=140,y=320)
+Button(base, text="Delete", command= sftp_delete, width=12,bg="brown",fg='white').place(x=250,y=320)
+Button(base, text="Close", command= close, width=12,bg="brown",fg='white').place(x=360,y=320)
 
 base.mainloop()  
--- sftp_module.py
+++ sftp_module.py
@@ -1,6 +1,7 @@
 import pysftp as sftp
 import os 
 from datetime import date
+from pathlib import Path
 
 class SftpModule:
     def __init__(self,FTP_HOST, FTP_USER, FTP_PASS, PORT=22):
@@ -11,52 +12,73 @@ class SftpModule:
         cnopts = sftp.CnOpts()
         cnopts.hostkeys = None
         self.sftp = sftp.Connection(host=self.host, port=self.port, username=self.user, password=self.password, cnopts=cnopts)
+        self.base_dir = Path(os.getcwd())
         print("connection established successfully")
 
-    def download_sftp(self,local_folder_name=None, sftp_file_name=None):
+    def download_sftp(self,local_file_path=None, sftp_file_path=None):
         try:
             serv_details = self.sftp
             current_dir = serv_details.pwd
 
-            if local_folder_name!= None:
-                local_file_path = local_folder_name
+            if local_file_path!= None:
+                local_folder = local_file_path
             else:
-                local_file_path = os.getcwd()
+                local_folder = os.getcwd()
             print(current_dir)
-            if sftp_file_name!= None:
-                remote_file_path = current_dir + '/' + sftp_file_name
-                local_file = local_file_path + '/' + sftp_file_name
-                serv_details.get(remote_file_path, local_file)
-                print(local_file,'downloaded successfully')
 
+            if sftp_file_path!= None and serv_details.exists(sftp_file_path):
+                if serv_details.isdir(sftp_file_path):
+                    local_file = os.path.join(local_folder, sftp_file_path)
+                    if not os.path.exists(local_file):
+                        os.makedirs(local_folder +'/'+ sftp_file_path)
+                    serv_details.get_d(sftp_file_path, local_file, preserve_mtime=True)
+
+                if serv_details.isfile(sftp_file_path):
+                    remote_file_path = current_dir + '/' + sftp_file_path
+                    file = os.path.split(sftp_file_path)[-1]
+                    local_file = local_folder + '/' + file
+                    serv_details.get(remote_file_path, local_file, preserve_mtime=True)
+
+                print(sftp_file_path,'downloaded successfully')
+                return 'File downloaded successfully'
             else:
-                print('specified file not found')
+                return 'specified file not found'
+       
         except Exception as e:
             raise e
 
-    def upload_sftp(self, local_file_name =None, sftp_folder_name=None):
+    def upload_sftp(self, local_file_name = None, sftp_folder_name=None):
         try:
-            cnopts = sftp.CnOpts()
-            cnopts.hostkeys = None
             serv_details = self.sftp
-            print("connection established successfully")
-            current_dir = serv_details.pwd
-            if local_file_name!= None or local_file_name!= '':
-                file_name = os.path.basename(local_file_name)
-                if sftp_folder_name!= None:
-                    if serv_details.exists(sftp_folder_name):
-                        remote_file_path = sftp_folder_name +'/'+ file_name
-                    else:
-                        serv_details.makedirs(sftp_folder_name)
-                        remote_file_path = sftp_folder_name +'/'+ file_name
-                else:
-                    remote_file_path = current_dir +'/'+ file_name
-                
-                serv_details.put(local_file_name, remote_file_path)
-                print(local_file_name,'uploaded successfully')
-
+            server_home_dir = serv_details.pwd
+            if sftp_folder_name!= None:
+                remote_file_path = server_home_dir + '/' + sftp_folder_name
+                if not serv_details.exists(remote_file_path):
+                    serv_details.makedirs(remote_file_path)
             else:
-                print('please provide valid file name')
+                remote_file_path = serv_details.pwd
+
+            if (local_file_name!= None) and (os.path.exists(local_file_name)):
+                if os.path.isdir(local_file_name):
+                    folder = os.path.split(local_file_name)[-1]
+                    remote_file_path = remote_file_path + '/' + folder
+                    if not serv_details.exists(remote_file_path):
+                        serv_details.makedirs(remote_file_path)
+
+                    for file in os.listdir(local_file_name):
+                        file_name = os.path.join(local_file_name, file)
+                        remote_file = remote_file_path + '/' + file
+                        serv_details.put(file_name, remote_file, preserve_mtime=True)   
+
+                if os.path.isfile(local_file_name):   
+                    file_name = os.path.split(local_file_name)[-1]
+                    remote_file =  remote_file_path +'/'+ file_name
+                    serv_details.put(local_file_name, remote_file, preserve_mtime=True)
+
+                print(local_file_name,'uploaded successfully')
+                return 'File uploaded successfully'
+            else:
+                return 'please provide valid file name'
         except Exception as e:
             raise e
 
@@ -81,6 +103,7 @@ class SftpModule:
                 for item in self.sftp.listdir(file_name):
                     self.sftp.remove(item) 
             self.sftp.remove(file_name)
+            return 'File removed successfully'
         except Exception as e:
             raise e
             
@@ -89,12 +112,12 @@ class SftpModule:
 
 if __name__ == "__main__":
     pass
-# download_sftp('data','myfile.zip')
-# FTP_HOST =  '10.55.196.70'
-# FTP_USER = 'mahammad138724'
-# FTP_PASS = 'Mar@2022'
+# # download_sftp('data','myfile.zip')
+# FTP_HOST =  '54.236.252.50'
+# FTP_USER = 'mdjuber'
+# FTP_PASS = 'Mjaas@31193'
 # port=22
 # obj = SftpModule(FTP_HOST,FTP_USER,FTP_PASS,port)
-# obj.upload_sftp(local_file_name = r"C:\Users\mahammad138724\Downloads\data.xlsx", sftp_folder_name='juber')
-# obj.download_sftp(sftp_file_name = '')
-# result = obj.sftp_get_list()
+# result = obj.upload_sftp(local_file_name = r"C:\Users\sarwa\Downloads\1672916981-test.xlsx", sftp_folder_name='juber/code')
+# # obj.download_sftp(sftp_file_path = 'output1.log')
+# # result = obj.sftp_get_list()
